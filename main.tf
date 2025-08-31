675d3d1ba1ea5ed012c93fe73b32d575bab47699
KIRAN <ks@gmail.com>
2025-08-31 11:53:30 +0530

    subnets

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ resource "aws_docdb_cluster" "docdb" {
   for_each = var.docdb
   cluster_identifier     = "${var.env}-${each.key}-roboshop-docdb"
   engine                 = each.value.engine
-  master_username        = "admin"
-  master_password        = "admin123"
+  master_username        = "admin1"
+  master_password        = "mustbeeightchars"
   skip_final_snapshot    = true
   db_subnet_group_name = "${var.env}-${each.key}-roboshop-docdb"
 }
