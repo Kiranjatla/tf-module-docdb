b9b99050f79b330b611a918036e407bcc894cfb9
KIRAN <ks@gmail.com>
2025-09-16 20:02:27 +0530

    Production Environment

--- schema.tf
+++ schema.tf
@@ -3,7 +3,6 @@ resource "null_resource" "load-schema" {
 
   provisioner "local-exec" {
     command = <<EOF
-sleep 60
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
 unzip -o mongodb.zip
